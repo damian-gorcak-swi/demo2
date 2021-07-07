0ff2c44358863fe59b93bbf12f9e3848bc6c0cb5
damian.gorcak <damian.gorcak@solarwinds.com>
2021-07-07 14:10:36 +0200

    addnig new print line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        //additive
         System.out.println("Hello world");
+        System.out.println("other commit");
     }
+
 }
